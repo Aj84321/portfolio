82965591853a43e051bb2123fd481072f049d76d
anand <149761038+anandcodexo@users.noreply.github.com>
2024-12-31 14:56:43 +0530

    c

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
+import HeroImg from '../../components/Hero/heroimg'
 
 const Home = () => {
     
   return (
     <>
         <Navbar/>
-        <div>Home</div>
+        <HeroImg/>
     </>
 
   )
