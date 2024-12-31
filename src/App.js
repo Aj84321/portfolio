50fce1884b2a83266ccc1cbcbabdd2a6e5578485
anand <149761038+anandcodexo@users.noreply.github.com>
2024-12-31 11:12:37 +0530

    chnges

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import About from "./container/About";
+import Contact from "./container/Contact";
+import Home from "./container/Home";
+import Project from "./container/Project";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/project' element={<Project />} />
+         <Route path='/contact' element={<Contact/>} />
+         <Route path='/about' element={<About/>} /> 
 
+        </Routes>
+      </Router>
     </div>
   );
 }
