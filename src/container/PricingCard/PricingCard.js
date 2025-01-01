5bbef521e436fedc5c2ecbae19e53472481afac9
anand <jainanand84321@gmail.com>
2025-01-01 14:36:46 +0530

    x

---
+++ src/container/PricingCard/PricingCard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./PricingCardStyle.css"
+import { Link } from "react-router-dom";
+
+const PricingCard = () => {
+    return (
+        <>
+        <div className="pricing">
+            <div className="card-container">
+                <div className="card">
+                    <h3>- Basic -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">5000 INR</p>
+                    <p>- 3 Days -</p>
+                    <p>- 8 Pages -</p>
+
+                    <p>-Featured-</p>
+                    <p>-Responsive Design-</p>
+                    <Link to="/contact" className="btn">
+                    PURCHASE NOW
+                    </Link>
+                </div>
+
+                <div className="card">
+                    <h3>- Premium -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">10000 INR</p>
+                    <p>- 2 Days -</p>
+                    <p>- 10 Days -</p>
+
+                    <p>-Featured-</p>
+                    <p>-Responsive Design-</p>
+                    <Link to="/contact" className="btn">
+                    PURCHASE NOW
+                    </Link>
+                </div>
+
+                <div className="card">
+                    <h3>- Buisness -</h3>
+                    <span className="bar"></span>
+                    <p className="btc">15000 INR</p>
+                    <p>- 1 Days -</p>
+                    <p>- 12 Days -</p>
+
+                    <p>-Featured-</p>
+                    <p>-Responsive Design-</p>
+                    <Link to="/contact" className="btn">
+                    PURCHASE NOW
+                    </Link>
+                </div>
+
+            </div>
+        </div>
+        <p>PricingCard</p>
+        </>
+    )
+
+}
+export default PricingCard
--- src/container/Project/index.js
+++ src/container/Project/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import HeroImgSect from '../../components/Hero/heroimgsect';
+import PricingCard from '../PricingCard/PricingCard';
 
 const Project = () => {
 
@@ -9,6 +10,7 @@ const Project = () => {
         <>
        <Navbar/>
        <HeroImgSect heading="Projects" text="Some of most recent works"/>
+       <PricingCard/>
         <Footer/>
         </>
     )
