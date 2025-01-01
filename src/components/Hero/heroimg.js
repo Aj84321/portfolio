eb1be2ce1d527a5622b9407aab7c4f993c7c8d59
anand <jainanand84321@gmail.com>
2025-01-01 13:30:48 +0530

    c

--- src/components/Hero/heroimg.js
+++ src/components/Hero/heroimg.js
@@ -14,10 +14,10 @@ const HeroImg = () => {
           ></img>
         </div>
         <div className="content">
-            <p>Hi,</p>
+            <p>HI, I'm a Freelancer.</p>
             <h1>React Developer</h1>
             <div>
-                <Link to="/projects" className="btn">Projects</Link>
+                <Link to="/project" className="btn">Projects</Link>
                 <Link to="/contact" className="btn btn-light">Contact</Link>
             </div>
 
