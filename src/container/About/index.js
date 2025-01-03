ab5449523659d5197ab9f1d63992ab7c2ea7f8ab
anand <jainanand84321@gmail.com>
2025-01-03 14:04:34 +0530

    n

--- src/container/About/index.js
+++ src/container/About/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import HeroImgSect from "../../components/Hero/heroimgsect";
+import AboutContent from "../../components/AboutContent";
 
 const About = () => {
   return (
     <>
       <Navbar />
       <HeroImgSect heading="About" text="I am a Front-end Developer"/>
-
+    <AboutContent/>
       <Footer />
     </>
   );
--- src/container/Contact/index.js
+++ src/container/Contact/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import HeroImgSect from "../../components/Hero/heroimgsect";
+import Form from "../../components/Form";
 
 const Contact = () => {
   return (
     <>
       <Navbar />
       <HeroImgSect heading="Contact" text="Lets have a Chat"/>
-
+      <Form/>
       <Footer />
     </>
   );
