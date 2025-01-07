8bc452764961cbef2228ced2000889e5f00e74c9
anand <jainanand84321@gmail.com>
2025-01-07 14:01:47 +0530

    c

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,16 +4,28 @@ import "./FormStyles.css";
 const Form = () => {
   return (
     <div className="form">
-      <form>
+      <form 
+        action="https://getform.io/f/ajjjwxda" 
+        method="POST"
+      >
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" name="name" required />
+        
         <label>Email</label>
-        <input type="text"></input>
+        <input type="email" name="email" required />
+        
         <label>Subject</label>
-        <input type="text"></input>
+        <input type="text" name="subject" />
+        
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
-        <button className="btn">Submit</button>
+        <textarea 
+          name="message" 
+          rows="6" 
+          placeholder="Type your message here" 
+          required 
+        />
+        
+        <button className="btn" type="submit">Submit</button>
       </form>
     </div>
   );
